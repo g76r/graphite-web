3aeb845f733b79ac5450dbcfdeb45b40dae6a8c8
Florent Couffe <florent.couffe.prestataire@sfr.com>
2013-09-11 15:06:59 +0200

    Patch for empty series
    When a function is called on an empty serie, the function 'normalize'
    encoutered an error.
    Now it raises a custom error that is tracked by the function 'evaluateTokens'
    and returns []

--- webapp/graphite/render/functions.py
+++ webapp/graphite/render/functions.py
@@ -126,9 +126,6 @@ def lcm(a, b):
   return a / gcd(a,b) * b
 
 def normalize(seriesLists):
-  step = 1
-  start = 0
-  end = 1
   seriesList = reduce(lambda L1,L2: L1+L2,seriesLists)
   if seriesList:
     step = reduce(lcm,[s.step for s in seriesList])
@@ -137,7 +134,17 @@ def normalize(seriesLists):
     start = min([s.start for s in seriesList])
     end = max([s.end for s in seriesList])
     end -= (end - start) % step
-  return (seriesList,start,end,step)
+    return (seriesList,start,end,step)
+  else:
+    raise NormalizeEmptyResultError()
+
+class NormalizeEmptyResultError(Exception):
+  """
+  Error thrown by the function 'normalize'
+  when it has an empty result
+  """
+  pass
+
 
 def formatPathExpressions(seriesList):
    # remove duplicates
