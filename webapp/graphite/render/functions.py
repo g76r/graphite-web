9997ae91cf8c159e7579f821de187cbb70951a7d
Mark Olson <theothermarkolson@gmail.com>
2014-08-26 15:23:33 -0400

    Add a pathExpression to constantLine

--- webapp/graphite/render/functions.py
+++ webapp/graphite/render/functions.py
@@ -2356,10 +2356,12 @@ def constantLine(requestContext, value):
     &target=constantLine(123.456)
 
   """
+  name = "constantLine(%s)" % str(value)
   start = timestamp( requestContext['startTime'] )
   end = timestamp( requestContext['endTime'] )
   step = int((end - start) / 2.0)
   series = TimeSeries(str(value), start, end, step, [value, value, value])
+  series.pathExpression = name
   return [series]
 
 def aggregateLine(requestContext, seriesList, func='avg'):
